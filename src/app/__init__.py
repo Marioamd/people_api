85bb5d7f0a7171973617cf625562865cc5cec5b7
Mario Ayme Medina <marioaymem@gmail.com>
2024-03-20 04:45:48 -0500

    prueba
    prueba

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -9,26 +9,25 @@ load_dotenv()
 
 app = Flask(__name__)
 
-KAFKA_BOOTSTRAP_SERVER = 'PLAINTEXT://0.tcp.ngrok.io:15554'
+KAFKA_BOOTSTRAP_SERVERS = 'PLAINTEXT://0.tcp.sa.ngrok.io:13779'
 KAFKA_TOPIC = 'mi_topico'
 
 def create_topic():
     # Este método crea un tópico si no existe
-    p = Producer({'bootstrap.servers': KAFKA_BOOTSTRAP_SERVER})
+    p = Producer({'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS})
     p.produce(KAFKA_TOPIC, key=None, value='', callback=lambda err, msg: print(msg) if err is None else print(err))
     p.flush()
 
-
 def send_message(message):
     # Este método envía un mensaje al tópico especificado
-    p = Producer({'bootstrap.servers': KAFKA_BOOTSTRAP_SERVER})
+    p = Producer({'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS})
     p.produce(KAFKA_TOPIC, message.encode('utf-8'), callback=lambda err, msg: print(msg) if err is None else print(err))
     p.flush()
 
 @app.route('/enviar-mensaje', methods=['POST'])
 def enviar_mensaje():
     content = request.json
-    message=json.dumps(content)
+    message = json.dumps(content)
     send_message(message)
     return jsonify({'message': 'Mensaje enviado exitosamente'})
 
