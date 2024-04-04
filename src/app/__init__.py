ffe14dbb91660cce1d4befa7e4b8a017427f6ce5
Mario Ayme Medina <marioaymem@gmail.com>
2024-04-04 17:24:33 -0500

    Cambios
    se añade ngrok

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,6 +8,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
